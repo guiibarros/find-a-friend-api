8564cc83e8b53f9a6dd2c661c729b1d2e42b4945
Guilherme <gui.barros.js@gmail.com>
2023-08-04 20:56:27 -0300

    feat: create register use case factory and prisma org repository

---
+++ src/repositories/prisma/prisma-orgs-repository.ts
@@ -0,0 +1,35 @@
+import { Prisma } from '@prisma/client'
+
+import { prisma } from '@/lib/prisma'
+
+import { OrgsRepository } from '../orgs-repository'
+
+export class PrismaOrgsRepository implements OrgsRepository {
+  async findById(id: string) {
+    const org = await prisma.org.findUnique({
+      where: {
+        id,
+      },
+    })
+
+    return org
+  }
+
+  async findByPhone(phone: string) {
+    const org = await prisma.org.findUnique({
+      where: {
+        phone,
+      },
+    })
+
+    return org
+  }
+
+  async create(data: Prisma.OrgCreateInput) {
+    const org = await prisma.org.create({
+      data,
+    })
+
+    return org
+  }
+}
