df00d3572cea219f252ade90877fab36a6c7e37a
Guilherme <gui.barros.js@gmail.com>
2023-08-07 22:57:06 -0300

    feat: create prisma test environment

---
+++ prisma/vitest-environment-prisma/prisma-test-environment.mjs
@@ -0,0 +1,40 @@
+import 'dotenv/config'
+
+import { PrismaClient } from '@prisma/client'
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
+
+const prisma = new PrismaClient()
+
+function generateDatabaseURL(schema) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Provide a DATABASE_URL environment variable.')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
+
+export default {
+  name: 'prisma',
+  transformMode: 'ssr',
+  async setup() {
+    const schema = randomUUID()
+    const databaseURL = generateDatabaseURL(schema)
+
+    process.env.DATABASE_URL = databaseURL
+    execSync('npx prisma migrate deploy')
+
+    return {
+      async teardown() {
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+
+        await prisma.$disconnect()
+      },
+    }
+  },
+}
